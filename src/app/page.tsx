fd1fd3eaae8991a00d608b5c364074967032d9f2
Abdelrahman Hafez <dahomy999@outlook.sa>
2025-02-06 11:59:43 +0200

    updated hero background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,23 @@ export default async function Home() {
             <div className="absolute inset-0">
               <Image
                 className="h-full w-full object-cover"
-                src="/hero-background.jpg"
+                src="/hero-background.png"
                 alt="StrawPoll Splash Image"
                 layout="fill"
                 objectFit="cover"
               />
-              <div className="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"></div>
+              {/* <div className="absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"></div> */}
             </div>
             <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:pt-28 lg:pb-32 lg:px-8">
               <h1 className="text-center sm:text-left text-5xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl sm:ml-12">
                 <span className="block text-white">Create a poll</span>
                 <span className="block text-indigo-300">in seconds</span>
               </h1>
+
+              <div className="hidden sm:block ml-12 text-lg mt-4 text-indigo-200">
+                No signup required
+              </div>
+
               <p className="mt-6 max-w-sm mx-auto sm:ml-12 text-center sm:text-left text-lg sm:text-xl text-indigo-200 sm:max-w-2xl">
                 Want to ask your friends where to go friday night or arrange a
                 meeting with co-workers? Create a poll - and get answers in no
@@ -36,9 +41,6 @@ export default async function Home() {
                     Create a poll
                   </Link>
                 </div>
-              </div>
-              <div className="hidden sm:block ml-12 text-sm mt-4 text-indigo-200">
-                No signup required
               </div>
             </div>
           </div>
